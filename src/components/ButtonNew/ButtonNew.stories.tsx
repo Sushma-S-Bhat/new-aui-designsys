fbe24209edd484030882377e95337e74e289327d
sushma bhat <sushma@aglinthq.com>
2023-05-27 13:22:41 +0530

    added new component dtory

---
+++ src/components/ButtonNew/ButtonNew.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import AuiButton from './ButtonNew'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof AuiButton> = {
+  title: 'Example/Button',
+  component: AuiButton,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+}
+
+export default meta
+type Story = StoryObj<typeof AuiButton>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const Primary: Story = {
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+  args: {
+    variant: 'contained',
+    label: 'Button',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'outlined',
+    label: 'Button',
+  },
+}
+export const Tertiary: Story = {
+  args: {
+    variant: 'text',
+    label: 'Button',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    label: 'Button',
+    size: 'large',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    label: 'Button',
+    size: 'small',
+  },
+}
---
+++ src/components/ButtonNew/ButtonNew.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// import './button.css'
+import { Button } from '@mui/material'
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  variant?: 'contained' | 'outlined' | 'text'
+  // color?: string
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * Button contents
+   */
+  label?: string
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export default function AuiButton({ label, variant, size }: ButtonProps) {
+  // const mode = variant ? 'storybook-button--variant' : 'storybook-button--secondary'
+  return <Button variant={variant} size={size} > {label} </Button>
+}
