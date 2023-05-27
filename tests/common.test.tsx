6dbb7fae7d58479d9dae99a172b4d33879821441
sushma bhat <sushma@aglinthq.com>
2023-05-27 20:00:04 +0530

    fix: bug fix

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,10 +3,10 @@ import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import { MyCounter } from '../src'
+import { AuiButton } from '../src'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
-    render(<MyCounter />)
+    render(<AuiButton label='button' />)
   })
 })
