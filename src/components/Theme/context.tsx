7cb283c3e62ffebd356afa256f5f251fa86ad6e8
sushma bhat <sushma@aglinthq.com>
2023-05-29 10:57:04 +0530

    fix: commented the theme

--- src/components/Theme/context.tsx
+++ src/components/Theme/context.tsx
@@ -1,5 +1,5 @@
-import { createContext } from 'react'
+// import { createContext } from 'react'
 
-const ThemeContext = createContext(null)
+// const ThemeContext = createContext(null)
 
-export default ThemeContext;
+// export default ThemeContext;
